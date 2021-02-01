1126556eefdc64b16ef8ca14966a8ecdd93c5319
Jesus Reyes <jesusareyes01@gmail.com>
2021-02-01 23:19:59 +0000

    finished project using class component and state management

--- src/App.js
+++ src/App.js
@@ -2,15 +2,42 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const value1 = Math.floor(Math.random() * 100);
-const value2 = Math.floor(Math.random() * 100);
-const value3 = Math.floor(Math.random() * 100);
-const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
-const numQuestions = 0;
-const numCorrect = 0;
-
 class App extends Component {
+  state = {
+    numQuestions: 0,
+    numCorrect: 0
+  }
+  
   render() {
+    const value1 = Math.floor(Math.random() * 100);
+	const value2 = Math.floor(Math.random() * 100);
+	const value3 = Math.floor(Math.random() * 100);
+    const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
+
+    const checkAnswerTrue = () => {
+    	let sum = value1 + value2 + value3;
+
+        this.setState({...this.state, numQuestions: this.state.numQuestions + 1})
+
+        if (sum === proposedAnswer) {
+          this.setState((prevState) => ({
+            numCorrect: prevState.numCorrect + 1
+         })) 
+        }
+    }
+
+  	const checkAnswerFalse = () => {
+    	let sum = value1 + value2 + value3;
+      
+        this.setState({...this.state, numQuestions: this.state.numQuestions + 1})
+
+        if (sum !== proposedAnswer) {
+          this.setState((prevState) => ({
+            numCorrect: prevState.numCorrect + 1
+          })) 
+        }
+    }
+
     return (
       <div className="App">
         <header className="App-header">
@@ -22,10 +49,10 @@ class App extends Component {
           <div className="equation">
             <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
           </div>
-          <button>True</button>
-          <button>False</button>
+          <button onClick={checkAnswerTrue}>True</button>
+          <button onClick={checkAnswerFalse}>False</button>
           <p className="text">
-            Your Score: {numCorrect}/{numQuestions}
+            Your Score: {this.state.numCorrect}/{this.state.numQuestions}
           </p>
         </div>
       </div>
